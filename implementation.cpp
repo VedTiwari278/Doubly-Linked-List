7213445566bd5ad4ac28e9608090b7585a941235
VedTiwari278 <vedprakasht759@gmail.com>
2024-06-19 10:13:22 +0530

    Singly Linked list

--- implementation.cpp
+++ implementation.cpp
@@ -13,6 +13,13 @@ struct Node {
 };
 
 
+/* 
+   =======================
+   ALL INSERTION OPERAION 
+   =======================
+*/
+
+
 Node *head = NULL;
 
 /*   Insertion of item at the Head  */
@@ -63,7 +70,90 @@ void insert_at_Mid(int data){
    temp->next=newnode;
 }
 
+/*
+
+    ======================
+    ALL DELETION OPERATION
+    ======================
+
+*/
+
+/*
+  Deletion from the head/Start 
+*/
+void delete_from_head(){
+Node* temp=head;
+if(head->next==NULL){
+   cout<<"List is Empty now .";
+}
+head=temp->next;
+temp->next->prev=NULL;
+}
+
+/*
+   Deletion from the Tail/End
+*/
+void delete_from_End(){
+Node*temp=head;
+
+if(head->next==NULL){
+   cout<<"List is Empty now .";
+}
+
+while(temp->next!=NULL){
+   temp=temp->next;
+}
+temp->prev->next=NULL;
+}
+
+
+// Node* head = nullptr;
+
+void delete_from_Mid() {
+    int key;
+    cout << "Enter key to delete from the linked list: ";
+    cin >> key;
+
+    if (head == nullptr) {
+        cout << "List is empty. Cannot delete." << endl;
+        return;
+    }
+
+    Node* temp = head;
+
+    // Traverse the list to find the node with the given key
+    while (temp != nullptr && temp->data != key) {
+        temp = temp->next;
+    }
+
+    // If the key was not found
+    if (temp == nullptr) {
+        cout << "Key not found in the list." << endl;
+        return;
+    }
+
+    // If the node to be deleted is the last node
+    if (temp->next == nullptr) {
+        cout << "The key is at the end of the list. Nothing to delete after it." << endl;
+        return;
+    }
+
+    Node* node_to_delete = temp->next;
+
+    // Update the links to remove the node from the list
+    if (node_to_delete->next != nullptr) {
+        node_to_delete->next->prev = temp;
+    }
+    temp->next = node_to_delete->next;
+
+    // Free the memory of the node to be deleted
+    delete node_to_delete;
+
+    cout << "Node after the key has been deleted." << endl;
+}
+
 /*  Traversing of Doubly linked list */
+
 void Traversing(){
    Node* temp=head;
    cout<<"\nElements of Linked List : ";
@@ -78,7 +168,7 @@ int main()
 int con=1;
 while(con==1){
    cout<<"Choose your option : \n";
-   cout<<"1.Insert at beg \t 2.Insert at last \t 3.Insert at mid ";     // User choice accepted to implement singly linked list  
+   cout<<"1.Insert at beg \t 2.Insert at last \t 3.Insert at mid \t 4.Delete from head \t 5.Delete from End/Tail \t 6.Delete the given node";     // User choice accepted to implement singly linked list  
 int ch;
 cin>>ch;
 switch(ch){
@@ -86,21 +176,32 @@ switch(ch){
            int item;
            cout<<"Enter data to be inserted : ";
            cin>>item;
-           insert_at_beg(item);    // Beginning insertion 
+           insert_at_beg(item);        // Beginning insertion 
            Traversing();
            break;
     case 2: int value;
            cout<<"Enter data to be inserted : ";
            cin>>value;
-           insert_at_End(value); // Last insertion 
+           insert_at_End(value);      // Last insertion 
            Traversing();
             break;
 
     case 3:int mid;
            cout<<"Enter data to be inserted : ";
            cin>>mid;
-           insert_at_Mid(mid);   // Mid insertion
+           insert_at_Mid(mid);       // Mid insertion
            Traversing(); 
+           break;
+         
+    case 4:delete_from_head();      // Deletion from the head
+           Traversing();
+           break;
+    case 5:delete_from_End();       // Deletion from the End/Tail
+           Traversing();
+           break;
+         
+    case 6:delete_from_Mid();
+           Traversing();
            break;
     return 0;
 }
